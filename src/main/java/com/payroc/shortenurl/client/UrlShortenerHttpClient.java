43df090a677821e2be605c05369fc40ae04c5d9a
Wossenyeleh Abate <wossenyeleh.abate@vesta.io>
2024-02-06 04:17:03 +0000

    Second commit before 2nd interview adding more functioanlities and tests

--- src/main/java/com/payroc/shortenurl/client/UrlShortenerHttpClient.java
+++ src/main/java/com/payroc/shortenurl/client/UrlShortenerHttpClient.java
@@ -2,6 +2,7 @@ package com.payroc.shortenurl.client;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class UrlShortenerHttpClient {
 
@@ -10,28 +11,39 @@ public class UrlShortenerHttpClient {
 
 		//Keeping our thread pool to 2
 		final int threadNum = 2;
-		
+
 		//Runnable to get short url from original url
 		Runnable tinyUrlReturnRunnable = new PostRequest_ShortUrlGetRunnable();
-		
+
 		//Runnable to get original url from short url
 		Runnable origUrlGetRunnable = new GetRequest_OriginalUrlGetRunnable();
-		
+
 		//Runnable to list all entries in our database
 		Runnable listAllGetRunnable = new GetRequest_AllRecordsRunnable();
 
-		
+
 		//Instantiate executer using Executors factory 
 		ExecutorService executor = Executors.newFixedThreadPool(5);
-		
-		//Use all thread to execute our API calls
+
+		//Use all threads to execute our API calls
 		for(int i=0; i<threadNum; i++) {
-			executor.execute(tinyUrlReturnRunnable);
+			//executor.execute(tinyUrlReturnRunnable);
 			//executor.execute(origUrlGetRunnable);
-			//executor.execute(listAllGetRunnable);
+			executor.execute(listAllGetRunnable);
 		}
-		
-		
+
+
+		executor.shutdown();
+
+		//Finish existing tasks
+		try {
+			executor.awaitTermination(2, TimeUnit.MILLISECONDS);
+		} catch (InterruptedException e) {
+
+		}finally {
+			if(!executor.isTerminated())
+				executor.shutdown();
+		}
 
 	}
 }
